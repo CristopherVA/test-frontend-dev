1d86543e934a475f609592d0f2bf30c716efb9ba
CritopherVA <ing.valentin99@hotmail.com>
2024-04-15 12:40:35 -0400

    cahnge

--- lib/session.ts
+++ lib/session.ts
@@ -1,10 +1,16 @@
 import { SessionInterface } from "@/interface"
 
 export const addSession = (session: SessionInterface) => {
-    window.sessionStorage.setItem("session", JSON.stringify(session))
+    if (typeof window !== 'undefined') {
+        // do your stuff with sessionStorage
+        window.sessionStorage.setItem("session", JSON.stringify(session))
+    }
 }
 
 export const getSession = () => {
-    const session: SessionInterface = JSON?.parse(window.sessionStorage.getItem("session" || "") || "{}")
-    return session
+    if (typeof window !== 'undefined') {
+        // do your stuff with sessionStorage
+        const session: SessionInterface = JSON?.parse(window.sessionStorage.getItem("session" || "") || "{}")
+        return session
+    }
 }
